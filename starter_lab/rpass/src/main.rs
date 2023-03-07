3f4bbd66b78406080134aa3c01eee0cbdf25ccb6
Adrien Gras <72006541+AdrienGras@users.noreply.github.com>
2023-03-07 22:03:12 +0100

    prez prep

--- starter_lab/rpass/src/main.rs
+++ starter_lab/rpass/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use clap::Parser;
 use cli::Cli;
 use console_utils::ConsoleIO;
@@ -11,8 +13,11 @@ pub mod middleware;
 pub mod passwords;
 pub mod utils;
 
+#[allow(unreachable_code)]
 fn main() {
     let cli = Cli::parse();
+
+    todo!();
 
     if let Err(error) = middleware::handle(&cli) {
         let console = ConsoleIO::new();
